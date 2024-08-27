bee59753c4cc3233a3b6ddc9d14b4bae18c073c4
Amy Lopez <amy.lopez@sainsburysbank.co.uk>
2024-08-27 21:56:26 +0100

    add formality

--- src/components/Phrase.jsx
+++ src/components/Phrase.jsx
@@ -1,8 +1,11 @@
-export const Phrase = ({ category, wolof, english }) => {
+export const Phrase = ({ category, wolof, english, informal }) => {
   return (
     <div className="phrase">
       <div className="category-name">{category}</div>
-      <div className="wolof-phrase">{wolof}</div>
+
+      <div className="wolof-phrase">
+        {wolof} {informal && <span className="formality">informal</span>}
+      </div>
       <div className="english-phrase">{english}</div>
     </div>
   );
